94d0917bb03b69ffaba87aa4f6c4aa22b13752db
Jorge Eduardo <jeduoliveira81@gmail.com>
2022-03-08 12:27:38 -0300

    first commit

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
 
 apt update -y
-apt upgrade -y 
-apt install git -y
 apt full-upgrade -y
-
 apt install python3-dev git awscli -y
 apt install -y python3 python3-pip
 apt install libffi-dev libssl-dev -y
@@ -18,7 +15,7 @@ baixar o arquivo yolov4.weights em  https://drive.google.com/open?id=1cewMfusmPj
 
 python3 -m venv .venv 
 source .venv/bin/activate
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 python3 save_model.py --model yolov4 
-python main.py --video 0  --model yolov4  
+python3 main.py --video 0  --model yolov4  
