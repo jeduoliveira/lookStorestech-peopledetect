6830baca8cf694912d0a9822999f700984c08262
Jorge Eduardo <jeduoliveira81@gmail.com>
2022-03-11 23:17:19 -0300

    criando o install.sh

--- install.sh
+++ install.sh
@@ -32,10 +32,21 @@ do_install() {
 
     case "$lsb_dist" in
         debian|raspbian)
-			$sh_c 'apt-get update -qq >/dev/null'
-			#$sh_c 'apt-get upgrade -y >/dev/null'
-			$sh_c 'apt-get install python3-dev python3 python3-pip -y >/dev/null'
-			$sh_c 'apt-get install git awscli -y >/dev/null'
+			$sh_c 'systemctl enable ssh'
+			$sh_c 'systemctl start ssh'
+			$sh_c 'apt-get update '
+			$sh_c 'apt-get upgrade -y'
+			$sh_c 'apt-get install python3-dev python3 python3-pip -y'
+			$sh_c 'apt-get install git awscli -y '
+			$sh_c 'apt-get autoremove -y '
+			$sh_c '[ ! -d "./lookStorestech-peopledetect" ] &&  git clone https://github.com/jeduoliveira/lookStorestech-peopledetect.git'
+			$sh_c 'cd lookStorestech-peopledetect'
+			$sh_c '[ ! -d ".venv" ] && python3 -m venv .venv'
+			$sh_c 'source .venv/bin/activate'
+			$sh_c 'pip3 install -r requirements.txt'
+
+
+			
 		;;
         *)
 			echo "Error: ${lsb_dist} não suportado"
