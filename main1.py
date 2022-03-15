1b639954071c2a888881cb9c8c2bbb401ab8ab0c
Jorge Eduardo <jeduoliveira81@gmail.com>
2022-03-15 10:20:43 -0300

    testando

--- main1.py
+++ main1.py
@@ -5,7 +5,7 @@ captura = cv2.VideoCapture("tcp://192.168.0.118:8888")
 
 while(1):
     ret, frame = captura.read()
-    cv2.imshow("Video", frame)
+    #cv2.imshow("Video", frame)
    
     if cv2.waitKey(1) & 0xFF == ord('q'): break
 
